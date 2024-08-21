e85242e5675d573ea29647b8ba4007128800d3d6
CYBER DEXTER <152212325+darkewing@users.noreply.github.com>
2024-08-21 07:24:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,8 +23,8 @@ global.location = '1' //Ganti Pake Location Panel Lu
 //===========================//
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['2348140825959']
-global.ownMain = '2348140825959'
+global.owner = ['94785274495']
+global.ownMain = '94785274495'
 global.NamaOwner = '𝓑𝓐𝓓𝓑𝓞𝓘' //gausah diganti 
 global.sessionName = 'badboisession'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
